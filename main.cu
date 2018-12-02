0d251f31fa9034187618495d4a5507d38206fdf0
Chauncey <chaunceykiwi@gmail.com>
2018-12-02 13:04:23 -0800

    Add ch02-cuda

--- main.cu
+++ main.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <time.h>
+#include "vec3.h"
 using namespace std;
 
 // limited version of checkCudaErrors from helper_cuda.h in CUDA examples 
@@ -16,16 +17,14 @@ void check_cuda(cudaError_t result, char const *const func,
   }
 }
 
-__global__ void render(float *fb, int max_x, int max_y) {
+__global__ void render(vec3 *fb, int max_x, int max_y) {
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
   if ((i >= max_x) || (j >= max_y)) {
     return;
   }
-  int pixel_index = j * max_x * 3 + i * 3;
-  fb[pixel_index + 0] = float(i) / max_x;
-  fb[pixel_index + 1] = float(j) / max_y;
-  fb[pixel_index + 2] = 0.2;
+  int pixel_index = j * max_x + i;
+  fb[pixel_index] = vec3(float(i) / max_x, float(j) / max_y, 0.2f);
 }
 
 int main() {
@@ -38,10 +37,10 @@ int main() {
   std::cerr << "in " << tx << "*" << ty << " blocks." << endl;
 
   int num_pixels = nx * ny;
-  size_t fb_size = 3 * num_pixels * sizeof(float);
+  size_t fb_size = num_pixels * sizeof(vec3);
 
   // allocate FB
-  float *fb;
+  vec3 *fb;
   checkCudaErrors(cudaMallocManaged((void **)&fb, fb_size));
 
   clock_t start, stop;
@@ -56,16 +55,14 @@ int main() {
   double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
   cerr << "took " << timer_seconds << " seconds.\n";
 
+  // Output FB as Image
   cout << "P3\n" << nx << " " << ny << "\n255\n";
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      size_t pixel_index = j * 3 * nx + i * 3;
-      float r = fb[pixel_index + 0];
-      float g = fb[pixel_index + 1];
-      float b = fb[pixel_index + 2];
-      int ir = int(255.99 * r);
-      int ig = int(255.99 * g);
-      int ib = int(255.99 * b);
+      size_t pixel_index = j * nx + i;
+      int ir = int(255.99 * fb[pixel_index + 0].r());
+      int ig = int(255.99 * fb[pixel_index + 0].g());
+      int ib = int(255.99 * fb[pixel_index + 0].b());
       cout << ir << " " << ig << " " << ib << endl;
     }
   }
