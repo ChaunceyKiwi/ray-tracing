f5d8fa2c290ac5d25a4b7aaf3eb7ab39bb49b515
Chauncey <chaunceykiwi@gmail.com>
2018-12-02 18:15:54 -0800

    Add ch10-cuda

--- main.cu
+++ main.cu
@@ -90,7 +90,7 @@ __global__ void render(vec3 *fb, int max_x, int max_y, int ns, camera **cam,
 }
 
 __global__ void create_world(hitable **d_list, hitable **d_world, 
-                              camera **d_camera) {
+                              camera **d_camera, int nx, int ny) {
   if (threadIdx.x == 0 && blockIdx.x == 0) {
     d_list[0] = new sphere(vec3(0, 0, -1), 0.5,
                           new lambertian(vec3(0.1, 0.2, 0.5)));
@@ -103,7 +103,8 @@ __global__ void create_world(hitable **d_list, hitable **d_world,
     d_list[4] = new sphere(vec3(-1, 0, -1), -0.45,
                           new dielectric(1.5));
     *d_world = new hitable_list(d_list, 5);
-    *d_camera = new camera();
+    *d_camera = new camera(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0),
+                            20.0, float(nx) / float(ny));
   }
 }
 
@@ -148,7 +149,7 @@ int main() {
   checkCudaErrors(cudaMalloc((void **)&d_world, sizeof(hitable *)));
   camera **d_camera;
   checkCudaErrors(cudaMalloc((void **)&d_camera, sizeof(camera *)));
-  create_world<<<1, 1>>>(d_list, d_world, d_camera);
+  create_world<<<1, 1>>>(d_list, d_world, d_camera, nx, ny);
   checkCudaErrors(cudaGetLastError());
   checkCudaErrors(cudaDeviceSynchronize());
 
--- vec3.h
+++ vec3.h
@@ -95,6 +95,12 @@ __host__ __device__ inline float dot(const vec3 &v1, const vec3 &v2) {
   return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
 
+__host__ __device__ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+    return vec3((v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1]),
+                (-(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0])),
+                (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
+}
+
 __host__ __device__ inline vec3 &vec3::operator+=(const vec3 &v) {
   this->e[0] += v.e[0];
   this->e[1] += v.e[1];
