3bdda67caf74c98640aafa81f5147accd5e1d398
Chauncey <chaunceykiwi@gmail.com>
2019-04-21 16:11:41 -0700

    Add example of lights

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include "camera.h"
 #include "float.h"
 #include "hitable_list.h"
+#include "image_texture.h"
 #include "material.h"
 #include "moving_sphere.h"
 #include "sphere.h"
-#include "image_texture.h"
+#include "aarect.h"
 using namespace std;
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -16,17 +17,15 @@ vec3 color(const ray& r, hitable* world, int depth) {
   if (world->hit(r, 0.001, MAXFLOAT, rec)) {
     ray scattered;
     vec3 attenuation;
-
+    vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
     if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-      return attenuation * color(scattered, world, depth + 1);
+      return emitted + attenuation * color(scattered, world, depth + 1);
     } else {
-      return vec3(0, 0, 0);
+      return emitted;
     }
 
   } else {
-    vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    return vec3(0, 0, 0);
   }
 }
 
@@ -76,7 +75,8 @@ hitable* random_scene() {
 
 hitable* earth() {
   int nx, ny, nn;
-  unsigned char* tex_data = stbi_load("./assets/earthmap.jpg", &nx, &ny, &nn, 0);
+  unsigned char* tex_data =
+      stbi_load("./assets/earthmap.jpg", &nx, &ny, &nn, 0);
   material* mat = new lambertian(new image_texture(tex_data, nx, ny));
   return new sphere(vec3(0, 0, 0), 2, mat);
 }
@@ -89,15 +89,27 @@ hitable* two_perlin_spheres() {
   return new hitable_list(list, 2);
 }
 
+hitable* simple_light() {
+  texture* pertext = new noise_texture(4);
+  hitable** list = new hitable*[4];
+  list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
+  list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
+  list[2] = new sphere(vec3(0, 7, 0), 2,
+                       new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+  list[3] = new xy_rect(3, 5, 1, 3, -2,
+                        new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+  return new hitable_list(list, 4);
+}
+
 int main() {
   int nx = 960;
   int ny = 640;
   int ns = 10;
   cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-  hitable* world = earth();
+  hitable* world = simple_light();
 
-  vec3 lookfrom(13, 2, 3);
+  vec3 lookfrom(26,4, 6);
   vec3 lookat(0, 0, 0);
   float dist_to_focus = 10.0;
   float apeture = 0.0;
