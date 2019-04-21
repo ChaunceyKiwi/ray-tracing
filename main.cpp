b1eabfc9bc288ea2c7bf38f68f2abf9c8fb91ccc
Chauncey <chaunceykiwi@gmail.com>
2019-04-21 15:05:25 -0700

    Add example for image texture mapping

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@
 #include "material.h"
 #include "moving_sphere.h"
 #include "sphere.h"
+#include "image_texture.h"
 using namespace std;
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 vec3 color(const ray& r, hitable* world, int depth) {
   hit_record rec;
@@ -26,65 +30,72 @@ vec3 color(const ray& r, hitable* world, int depth) {
   }
 }
 
-// hitable* random_scene() {
-//   int n = 500;
-//   hitable** list = new hitable*[n + 1];
-//   texture* checker =
-//       new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)),
-//                           new constant_texture(vec3(0.9, 0.9, 0.9)));
-//   list[0] = new sphere(vec3(0, -1000, 0), 1000,
-//                        new lambertian(checker));
-//   int i = 1;
+hitable* random_scene() {
+  int n = 500;
+  hitable** list = new hitable*[n + 1];
+  texture* checker =
+      new checker_texture(new constant_texture(vec3(0.2, 0.3, 0.1)),
+                          new constant_texture(vec3(0.9, 0.9, 0.9)));
+  list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(checker));
+  int i = 1;
 
-//   for (int a = -11; a < 11; a++) {
-//     for (int b = -11; b < 11; b++) {
-//       float choose_mat = drand48();
-//       vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
-//       if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
-//         if (choose_mat < 0.8) {  // diffuse
-//           list[i++] = new moving_sphere(
-//               center, center + vec3(0, 0.5 * drand48(), 0), 0.0, 1.0, 0.2,
-//               new lambertian(
-//                   new constant_texture(vec3(drand48() * drand48() * drand48(),
-//                                         drand48() * drand48() * drand48(),
-//                                         drand48() * drand48() * drand48()))));
-//         } else if (choose_mat < 0.95) {  // metal
-//           list[i++] = new sphere(
-//               center, 0.2,
-//               new metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()),
-//                              0.5 * (1 + drand48())),
-//                         0.5 * drand48()));
-//         } else {  // glass
-//           list[i++] = new sphere(center, 0.2, new dielectric(1.5));
-//         }
-//       }
-//     }
-//   }
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      float choose_mat = drand48();
+      vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+      if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+        if (choose_mat < 0.8) {  // diffuse
+          list[i++] = new moving_sphere(
+              center, center + vec3(0, 0.5 * drand48(), 0), 0.0, 1.0, 0.2,
+              new lambertian(new constant_texture(
+                  vec3(drand48() * drand48() * drand48(),
+                       drand48() * drand48() * drand48(),
+                       drand48() * drand48() * drand48()))));
+        } else if (choose_mat < 0.95) {  // metal
+          list[i++] = new sphere(
+              center, 0.2,
+              new metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()),
+                             0.5 * (1 + drand48())),
+                        0.5 * drand48()));
+        } else {  // glass
+          list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+        }
+      }
+    }
+  }
 
-//   list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
-//   list[i++] = new sphere(vec3(-4, 1, 0), 1.0,
-//                          new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
-//   list[i++] =
-//       new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+  list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+  list[i++] =
+      new sphere(vec3(-4, 1, 0), 1.0,
+                 new lambertian(new constant_texture(vec3(0.4, 0.2, 0.1))));
+  list[i++] =
+      new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
 
-//   return new hitable_list(list, i);
-// }
+  return new hitable_list(list, i);
+}
 
-hitable *two_perlin_spheres() {
-  texture *pertext = new noise_texture(4);
-  hitable **list = new hitable*[2];
+hitable* earth() {
+  int nx, ny, nn;
+  unsigned char* tex_data = stbi_load("./assets/earthmap.jpg", &nx, &ny, &nn, 0);
+  material* mat = new lambertian(new image_texture(tex_data, nx, ny));
+  return new sphere(vec3(0, 0, 0), 2, mat);
+}
+
+hitable* two_perlin_spheres() {
+  texture* pertext = new noise_texture(4);
+  hitable** list = new hitable*[2];
   list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
   list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
   return new hitable_list(list, 2);
 }
 
 int main() {
-  int nx = 480;
-  int ny = 320;
-  int ns = 5;
+  int nx = 960;
+  int ny = 640;
+  int ns = 10;
   cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-  hitable* world = two_perlin_spheres();
+  hitable* world = earth();
 
   vec3 lookfrom(13, 2, 3);
   vec3 lookat(0, 0, 0);
