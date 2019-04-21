ac256e4ba479f2e0057232a27524589dbb353852
Chauncey <chaunceykiwi@gmail.com>
2019-04-20 22:33:23 -0700

    Add perlin noise example2

--- texture.h
+++ texture.h
@@ -1,7 +1,7 @@
 #ifndef TEXTUREH
 #define TEXTUREH
 
-#include  "perlin.h"
+#include "perlin.h"
 
 class texture {
  public:
@@ -19,7 +19,7 @@ class constant_texture : public texture {
 class checker_texture : public texture {
  public:
   checker_texture() {}
-  checker_texture(texture * t0, texture *t1): even(t0), odd(t1) {}
+  checker_texture(texture* t0, texture* t1) : even(t0), odd(t1) {}
   virtual vec3 value(float u, float v, const vec3& p) const {
     float sines = sin(10 * p.x()) * sin(10 * p.y()) * sin(10 * p.z());
     if (sines < 0) {
@@ -28,17 +28,19 @@ class checker_texture : public texture {
       return even->value(u, v, p);
     }
   }
-  texture *odd;
-  texture *even;
+  texture* odd;
+  texture* even;
 };
 
 class noise_texture : public texture {
  public:
-  noise_texture(){}
-  virtual vec3 value(float u , float v, const vec3& p) const {
-    return vec3(1, 1, 1) * noise.noise(p);
+  noise_texture() {}
+  noise_texture(float sc) : scale(sc) {}
+  virtual vec3 value(float u, float v, const vec3& p) const {
+    return vec3(1, 1, 1) * noise.noise(scale * p);
   }
   perlin noise;
+  float scale;
 };
 
 #endif
