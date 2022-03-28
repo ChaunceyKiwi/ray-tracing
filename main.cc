3cfa4728a90083351377075ceb99caef235b9e72
ChaunceyKiwi <chaunceykiwi@gmail.com>
2022-03-27 22:10:50 -0700

    Add vec3

--- main.cc
+++ main.cc
@@ -2,28 +2,28 @@
 using namespace std;
 
 int main() {
-    // Image
-    const int image_width = 256;
-    const int image_height = 256;
+  // Image
+  const int image_width = 256;
+  const int image_height = 256;
 
-    // Render
-    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+  // Render
+  cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height - 1; j >= 0; j--) {
-        cerr << "\rScanlines remaining: " << j << ' ' << flush;
+  for (int j = image_height - 1; j >= 0; j--) {
+    cerr << "\rScanlines remaining: " << j << ' ' << flush;
 
-        for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
+    for (int i = 0; i < image_width; i++) {
+      auto r = double(i) / (image_width - 1);
+      auto g = double(j) / (image_height - 1);
+      auto b = 0.25;
 
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
+      int ir = static_cast<int>(255.999 * r);
+      int ig = static_cast<int>(255.999 * g);
+      int ib = static_cast<int>(255.999 * b);
 
-            cout << ir << " " << ig << " " << ib << '\n';
-        }
+      cout << ir << " " << ig << " " << ib << '\n';
     }
+  }
 
-    cerr << "\nDone.\n";
+  cerr << "\nDone.\n";
 }
