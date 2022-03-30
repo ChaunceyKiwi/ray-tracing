417cda26631add7b6b6ee79b7b595c67b2fb99d4
ChaunceyKiwi <chaunceykiwi@gmail.com>
2022-03-29 18:14:09 -0700

    Add vector v1

--- color.h
+++ color.h
@@ -8,9 +8,9 @@ using namespace std;
 
 void write_color(ostream &out, color pixel_color) {
   // Write the translated [0, 255] value of each color component.
-  out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-      << static_cast<int>(255.999 * pixel_color.y()) << ' '
-      << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+  out << (int)(255.999 * pixel_color.x()) << ' '
+      << (int)(255.999 * pixel_color.y()) << ' '
+      << (int)(255.999 * pixel_color.z()) << '\n';
 }
 
 #endif
