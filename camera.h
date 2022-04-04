940e09dbbd1b67e2aefbd3c7886b553a36543b69
ChaunceyKiwi <chaunceykiwi@gmail.com>
2022-04-03 23:53:13 -0700

    Add camera orientation

--- camera.h
+++ camera.h
@@ -11,18 +11,21 @@ class camera {
   vec3 vertical;
 
  public:
-  camera(double vfov,  // vertical field-of-view in degress
+  camera(point3 lookfrom, point3 lookat, vec3 vup,
+         double vfov,  // vertical field-of-view in degress
          double aspect_ratio) {
     auto theta = degrees_to_radians(vfov);
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0 * tan(theta / 2) * focal_length;
+    auto viewport_height = 2.0 * tan(theta / 2);
     auto viewport_width = aspect_ratio * viewport_height;
 
-    origin = point3(0, 0, 0);
-    horizontal = vec3(viewport_width, 0, 0);
-    vertical = vec3(0, viewport_height, 0);
-    lower_left_corner =
-        origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    auto w = unit_vector(lookfrom - lookat);
+    auto u = unit_vector(cross(vup, w));
+    auto v = cross(w, u);
+
+    origin = lookfrom;
+    horizontal = viewport_width * u;
+    vertical = viewport_height * v;
+    lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
   }
 
   ray get_ray(double u, double v) {
